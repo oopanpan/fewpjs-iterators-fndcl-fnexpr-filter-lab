7985b74844f3e79f41c80068c17a4cc5a34d1e8d
oopanpan <oopanpan@gmail.com>
2021-02-28 01:40:28 -0800

    commit tree 26ab142b9ef40ac43f0168b4db4b8dbf4ca969ea

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+const findMatching = (drivers, string) =>{
+    let result =  drivers.filter( driver =>{
+        return driver.toUpperCase() === string.toUpperCase()
+    })
+    return result
+}
+
+const fuzzyMatch = (drivers, string) =>{
+    let result = drivers.filter( driver =>{
+        return driver.charAt(0) === string.charAt(0)
+    })
+    return result
+}
+
+const matchName = (drivers, string) =>{
+    let result = drivers.filter( driver =>{
+        return driver.name === string
+    })
+    return result
+}
